efac12dc7f7b997159592dbb9dbda89b4e0fa3b3
Miguel Aguiar <91989152+MiguelosAguialos@users.noreply.github.com>
2022-10-12 13:17:29 -0300

    Add files via upload

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
 import pip._vendor.requests 
 
-requisicao = pip._vendor.requests.get("http://localhost:3000/")
-print(requisicao.json())
+while True:
+    requisicao = pip._vendor.requests.get("http://localhost:3000/data")
+    values = requisicao.json()
+    valor1 = values["num"]
+    print(valor1)
--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 const express = require('express')
-const server = express()
+const app = express()
+const http = require('http')
+const server = http.createServer(app);
 const fs = require('fs')
 const nunjucks = require('nunjucks')
-nunjucks.configure("views",{express: server,noCache: true})
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+nunjucks.configure("views",{express: app,noCache: true})
         
 const data = fs.readFileSync('./user.json', 'utf-8');
 const user = JSON.parse(data)
          
-server.use(express.static('public'))
+app.use(express.static('public'))
 
-const valor = 0
-
-server.get("/", function(req,res){
-    res.render('index.html', {valor})
-    user.num +=1
-    fs.writeFileSync('./user.json', JSON.stringify(user, null, 2), 'utf-8');
-    console.log(user)
+app.get("/", function(req,res){
+    res.render('index.html')
 })
-server.get("/data", function(req,res){
-    user.num +=1
-    fs.writeFileSync('./user.json', JSON.stringify(user, null, 2), 'utf-8');
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      const valor = msg
+      user.num = valor
+      fs.writeFileSync('./user.json', JSON.stringify(user, null, 2), 'utf-8');
+      console.log("Valor enviado: "+msg)
+    });
+});
+
+app.get("/data", function(req,res){
     return res.json(user)
 })
 
-console.log('server run port 3000');
 server.listen(3000)
